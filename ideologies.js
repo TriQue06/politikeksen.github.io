15c6d074210150a6eff75685c0356ac73f8e19f8
TriQue06 <baristanlik@gmail.com>
2024-04-18 21:47:47 +0300

    Initial commit

---
+++ ideologies.js
@@ -0,0 +1,121 @@
+ideologies = [
+    {
+        "name": "Anarko-Komünizm",
+        "stats": {
+            "socialist_capitalist": 100,
+            "security_freedom": 0
+        }
+    },
+    {
+        "name": "Anarko-Kapitalizm",
+        "stats": {
+            "socialist_capitalist": 0,
+            "security_freedom": 0
+        }
+    },
+    {
+        "name": "Komünizm",
+        "stats": {
+            "socialist_capitalist": 100,
+            "security_freedom": 100
+        }
+    },
+    {
+        "name": "Monarşizm",
+        "stats": {
+            "socialist_capitalist": 0,
+            "security_freedom": 100
+        }
+    },
+    {
+        "name": "Demokratik Sosyalizm",
+        "stats": {
+            "socialist_capitalist": 75,
+            "security_freedom": 25
+        }
+    },
+    {
+        "name": "Sosyal Demokrasi",
+        "stats": {
+            "socialist_capitalist": 58,
+            "security_freedom": 58
+        }
+    },
+    {
+        "name": "İlerlemecilik / Yeşil Siyaset",
+        "stats": {
+            "socialist_capitalist": 58,
+            "security_freedom": 42
+        }
+    },
+    {
+        "name": "Marksizm",
+        "stats": {
+            "socialist_capitalist": 100,
+            "security_freedom": 50
+        }
+    },
+    {
+        "name": "Liberal Sosyalizm",
+        "stats": {
+            "socialist_capitalist": 58,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Anarşizm",
+        "stats": {
+            "socialist_capitalist": 50,
+            "security_freedom": 100
+        }
+    },
+    {
+        "name": "Sosyalizm",
+        "stats": {
+            "socialist_capitalist": 75,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Muhafazakârlık ve Milliyetçilik",
+        "stats": {
+            "socialist_capitalist": 25,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Faşizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 100
+        }
+    },
+    {
+        "name": "Kapitalizm",
+        "stats": {
+            "socialist_capitalist": 0,
+            "security_freedom": 50
+        }
+    },
+    {
+        "name": "Liberalizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 50
+        }
+    },
+    {
+        "name": "Liberteryenizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Liberteryenizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 75
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,83 @@
+questions =
+    [[//Authoritarian-Libertarian`
+        [//4
+            "Direk (radikal) demokrasi en iyi yönetim biçimidir.",
+            "İnsanlar, kendi sorunlarına göre oy vermeliler.",
+            "Meclis ve milletvekilleri, iyi bir sistemdir ve bizi temsil eder."
+        ],
+        [//3
+            "Hükûmetin, çoğunluğun fikrine göre hareket etmesi önemlidir, yanlış bile olsa.",
+            "Daha fazla insana soruldukça daha doğru sonuçlara ulaşılır.",
+            "Benim görüşlerimi en iyi ben bilir ve temsil edebilirim."
+        ],
+        [//2
+            "Seçimler, hükûmete olumlu veya olumsuz mesaj vermenin en iyi yoludur.",
+            "Demokrasi en iyi sistem değildir fakat denediklerimiz arasında en kötüsü de değildir.",
+            "Herkesin oyu bir ve eşit olmalıdır."
+        ],
+        [//1
+            "Demokrasi, bir karar verme sürecinden daha fazlasıdır.",
+            "İnsanlar, yöneticilerini seçebilecek hakka ve güce sahip olmalıdır.",
+            "Hükûmet her zaman halkının yanında olmalıdır."
+        ],
+        [//-1
+            "Bir lider ne kadar güçlü ve kısmen otorite sahibiyse o kadar iyidir.",
+            "Çoğunluğun görüşü her zaman doğru olmayabilir.",
+            "Liderler güçlü ve kısmen de olsa otoriter olmalı, yoksa halk acı çeker."
+        ],
+        [//-2
+            "Hiyerarşik (toplumsal sınıflar)  düzen en iyisidir.",
+            "Toplum, liderler ve onları takip edenler olmadan var olamaz.",
+            "Demokrasi, azınlığın, çoğunluk tarafından ezilmesini kolaylaştırır."
+        ],
+        [//-3
+            "Halk çoğunlukla yanlış kararlar verir.",
+            "Kendi hedeflerimizi diğer grupların hedeflerinin üzerinde tutmamız önemlidir, demokratik olmasa bile.",
+            "Halk, siyaset yapamayacak kadar duygusal ve değişkendir."
+        ],
+        [//-4
+            "Seçimler, kaynak israfıdır.",
+            "Halka doğrudan hizmet etmek millete zarar verir.",
+            "Halk, liderini sorgusuz sualsiz takip etmeli ve sorgulamamalıdır."
+        ]],[//Equality-Markets`
+        [//4
+            "Komünizm, eğer doğru uygulanırsa, uygun bir yönetim şeklidir.",
+            "Herkesten yeteneğine göre, herkese ihtiyacına göre, temelde iyi bir fikirdir.",
+            "Yollar, elektrik ve kamu hizmetleri halka, çalışma aletleri işçilere ait olmalıdır."
+        ],
+        [//3
+            "Şirket baskısı, devlet baskısından çok daha endişe vericidir.",
+            "Yoksulların geçimini sağlamak için zenginlerin vergisi artırılmalıdır.",
+            "Fırsat eşitliği, ne olursa olsun, zorunlu olmalıdır."
+        ],
+        [//2
+            "Tek ödemeli evrensel (devletin karşıladığı) sağlık hizmetini destekliyorum.",
+            "Zenginler, ihtiyaç duyduklarından çok daha fazlasına sahipler.",
+            "Temel insan ihtiyaçları, sağlık hizmetleri gibi, ücretsiz ve erişilebilir olmalıdır."
+        ],
+        [//1
+            "Tüketiciyi korumak için devletin ekonomiye müdahalesi kabul edilebilir olmalıdır.",
+            "İnsanlar başarılı olmak için fırsat eşitliğine sahip olmalıdır.",
+            "Zengin ailelerde doğanlar, tembel ve vasıfsız olsalar da hak etmedikleri şekilde başarılı oluyorlar."
+        ],
+        [//-1
+            "İnsanlar, servetlerini sonraki nesillere bırakma hakkına sahip olmalıdır.",
+            "Hükûmetin ekonomiye müdahalesi, ekonomi için bir tehdittir.",
+            "Kapitalizm, diğer tüm alternatiflerden daha iyidir.",
+        ],
+        [//-2
+            "İşsizliği çözmek yerine enflasyonu ve bütçeyi korumak daha iyidir.",
+            "Kapitalizmin olmadığı ekonomiler çöker.",
+            "Devletin kendi sanayisin olması kötü bir fikirdir."
+        ],
+        [//-3
+            "Piyasa ne kadar özgürse insan o kadar özgür olur.",
+            "İnsan, elde edebileceği her şey için çalışmak zorunda olmalıdır.",
+            "Fakirler, fakir olmayı tercih eder."
+        ],
+        [//-4
+            "Hükûmetin ekonomiye müdahalesi kabul edilemez.",
+            "Devlet, tekellere dokunmamalıdır.",
+            "Çalışmayanlar ölmelidir."
+        ]
+    ]];
